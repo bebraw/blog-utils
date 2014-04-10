b675ce126a5d1ccc6c374472d9d51a5bdd566463
Juho Vepsalainen <bebraw@gmail.com>
2014-04-10 12:05:34 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# blog-utils
+
+Blog utils for fetching tweets, composing posts etc.
+
+## get_tweet_urls
+
+The tool fetches tweets associated to configuration account (possible to extend later). Still missing fetch since date.
+
+Usage: `./get_tweet_urls.js twitter_config.js > tweets.json`.
+
+Configuration:
+
+```
+module.exports = {
+    key: 'get via twitter dev site',
+    secret: 'get via twitter dev site',
+    token: 'get via twitter dev site',
+    tokenSecret: 'get via twitter dev site'
+};
+```
+
+## resolve_urls
+
+TODO. This should resolve urls to their full form. Idea: pipe and use request `res.request.uri.href`.
+
+## strip_url_parameters
+
+TODO: This should strip ? parts (url.resolve) out of list of given urls (pipe).
+
+## License
+
+MIT.
---
+++ get_tweet_urls.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var path = require('path');
+
+var fp = require('annofp');
+var first = fp.first;
+var filter = fp.filter;
+var id = fp.id;
+var prop = fp.prop;
+var zip = require('annozip');
+
+var Twitter = require('simple-twitter');
+
+
+main();
+
+function main() {
+    var config = process.argv[2];
+
+    if(!config) {
+        return console.error('Missing config!');
+    }
+
+    // TODO: parse date
+    config = require(path.resolve(config));
+
+    var twitter = connect(config);
+    var qs = '?count=200&trim_user=1&exclude_replies=true&include_rts=false';
+
+    twitter.get('statuses/user_timeline', qs, function(err, data) {
+        if(err) {
+            return console.error(err);
+        }
+
+        data = JSON.parse(data);
+
+        var parts = [
+            data.map(prop('id')),
+            data.map(prop('created_at')).map(toDate),
+            data.map(prop('entities')).map(prop('urls')).map(first).map(prop('expanded_url'))
+        ];
+        var result = zip.apply(null, parts).map(filter.bind(null, id)).filter(lengthEquals(parts.length)).map(function(v) {
+            return {
+                id: v[0],
+                date: v[1],
+                url: v[2]
+            };
+        });
+
+        // TODO: fetch tweets till given date is matched (use max_id)
+        // https://dev.twitter.com/docs/api/1.1/get/statuses/user_timeline
+
+        console.log(JSON.stringify(result, null, 4));
+    });
+}
+
+function connect(config) {
+    return new Twitter(config.key, config.secret, config.token, config.tokenSecret);
+}
+
+function lengthEquals(amt) {
+    return function(arr) {
+        return arr.length === amt;
+    };
+}
+
+function toDate(str) {
+    return new Date(str);
+}
